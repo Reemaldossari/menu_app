232a14f11cb91f72ee1dc9b2a4d194a1224ac0fc
Reemaldossari <92865481+Reemaldossari@users.noreply.github.com>
2021-12-27 17:04:37 +0300

    Add login/logout

--- app/src/main/java/com/example/menu_app/MainActivity.kt
+++ app/src/main/java/com/example/menu_app/MainActivity.kt
@@ -3,17 +3,52 @@ package com.example.menu_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import com.example.menu_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private var islogin = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+//       setContentView(binding.root)
     }
 
+    // this is fun for show menu
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.as_menu,menu)
         return true
-
-
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.Contact_us -> return true
+            R.id.Logout -> return true
+            R.id.Setting -> return true
+            R.id.login -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        var login = menu?.findItem(R.id.login)
+        var logout =menu?.findItem(R.id.Logout)
+
+        if (islogin) {
+            islogin =!islogin
+            login?.setVisible(true)
+            logout?.setVisible(false)
+        } else{
+            islogin = !islogin
+            login?.setVisible(false)
+            logout?.setVisible(true)
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
 }
